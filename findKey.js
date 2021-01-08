2b83e8281864cd9cc050819a7ca9acf556318b7e
Taylor <ivingstaylor@gmail.com>
2021-01-08 00:54:37 +0000

    updated findkeybyvalue and created findkey

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`✖✖✖ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let key = Object.keys(object)
+  for (let keys of Object.keys(object)) {
+  if (callback(object[keys])){
+    return keys
+  }
+}
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")// => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri")
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 2 },
+  "noma":      { stars: 3 },
+  "elBulli":   { stars: 4 },
+  "Ora":       { stars: 5 },
+  "Akelarre":  { stars: 6 }
+}, x => x.stars === 4), "Akelarre")
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -17,3 +17,13 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+const findKeyByValue = function (object, value) {
+  for (let key in object) {  // iterates over properties of object
+    if (object[key] === value) {   //object[key] => object[drama] => 'the wire'
+      return key
+    }
+  }
+}
+
