8697db73f982575ed8f23d5e3728607f9a050636
Taylor <ivingstaylor@gmail.com>
2021-01-11 17:30:31 +0000

    created test file and moved tests to said file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`✖✖✖ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const eqArrays = (original, comparison) => {
   if (original.length !== comparison.length) {
@@ -19,26 +13,4 @@ const eqArrays = (original, comparison) => {
 
 }
 
-
-// function to see if the return value from eqArrays() and "true" match
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} !== ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`✖✖✖ Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
-//[----------------Testing examples----------------]
-
-let testArray1 = [1, 2, 3, 4, 5, 6];
-let testArray2 = [1, 2, 3, 4, 5, 6];
-assertEqual(eqArrays(testArray1, testArray2), true);
-
-let testArray3 = [1, 2, 3, 4, 5, 6];
-let testArray4 = [1, 2, 3, 4, 5, "6"];
-assertEqual(eqArrays(testArray3, testArray4), false);
-
-let testArray5 = ["cat", "dog", "tea", "child"];
-let testArray6 = ["person", "man", "woman", "camera", "tv"];
-assertEqual(eqArrays(testArray5, testArray6), true);
+module.exports = eqArrays;
