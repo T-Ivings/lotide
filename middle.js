2ee1830afe796fdd63b96613caf3be0403dd33fd
Taylor <ivingstaylor@gmail.com>
2021-01-11 20:13:20 +0000

    created test file and moved tests to said file, exports middle

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const assertArraysEqual = function (original, comparison) {
-  const bool = eqArrays(original, comparison);
-  if (bool) {
-    return console.log(`✔✔✔ Assertion Passed: ${original} === ${comparison}`);
-  } else {
-    return console.log(`✖✖✖ Assertion Failed": ${original} !== ${comparison}`);
-  }
-};
-
-// assert equal function implementation
-const eqArrays = function(original, comparison) {
-  let isTrue = true;
-
-  for (let x = 0; x < original.length; x++) {
-    if (original[x] !== comparison[x]) {
-      isTrue = false;
-      return isTrue;
-    }
-  }
-  return isTrue;
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 
 const middle = function(array) {
@@ -35,15 +16,4 @@ const middle = function(array) {
 }
 
 
-//-----------Test--------------
-const testArray = [1, 2, 3, 4, 5, 6]
-const testArray1 = [1, 2, 3, 4, 5, 6]
-assertArraysEqual(middle(testArray), middle(testArray1));
-
-
-// const testArray2 = [1, 2, 3, 4]
-// console.log(middle(testArray2))
-// const testArray3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-// console.log(middle(testArray3))
-// const testArray4 = ["dog", "cat", "person", "camera", "television"]
-// console.log(middle(testArray4))
+ module.exports = middle;
