e27fa4becd9203e26712127be296abf1861a76cf
Taylor <ivingstaylor@gmail.com>
2021-01-11 17:15:39 +0000

    created test file for tail and moved test code into it

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`✖✖✖ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
-  let tail = array.slice(1, array.length);
-
-  return tail;
+  return array.slice(1, array.length);;
 };
 
-const result = tail([1, 2, 3, 4, 5, 6]);
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 5);
-assertEqual(result[0], 2);
-assertEqual(result1.length, 2);
-assertEqual(result1[1], "Labs");
+module.exports = tail;
+
